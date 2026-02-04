ce4805f52c21e95b68c23c875f55fb085f32b5c8
João Paulo <joaopaulo.skodowski@gmail.com>
2026-02-04 16:18:34 -0300

    Update app.controller.spec.ts

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -7,12 +7,14 @@ describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
+    
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    
   });
 
 });
